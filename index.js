715d17750a5dcd6d3f92f303221bacce377d6e5f
Alex Edwards <info@acedigitalagency.net>
2018-04-01 23:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
+<<<<<<< HEAD
   let node = document.getElementById('grand-node')
   let nextNode = node.children[0]
 
@@ -28,4 +29,12 @@ function deepestChild() {
     nextNode = node.children[0]
   }
   return node;
+=======
+  const div4 = document.getElementById('grand-node')
+  var x;
+  for (let i = 0; i < div4.length; i++) {
+    var x = div4[i].innerHTML;
+  }
+  return x;
+>>>>>>> 9341db48bc7e859ffc02528bd3f5e948147c6aa6
 }

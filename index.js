f6478880d00f3cd177f3d92cb37c6087668b60af
Alex Edwards <info@acedigitalagency.net>
2018-04-01 20:14:58 +0000

    Done.

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const div4 = document.getElementById('grand-node')
-  var x;
-  for (let i = 0; i < div4.length; i++) {
-  var x = div4[i].innerHTML;
-  return x;
-}
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+  return node;
 }
